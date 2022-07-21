e26e161355b3c1b594bce3abcfdbecd007ea13da
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-21 17:17:50 +0200

    add tags table

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Tag;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['html', 'css', 'js', 'vue', 'php', 'blade'];
+
+        foreach($tags as $tag) {
+            $newTag = new Tag();
+
+            $newTag->name = $tag;
+            $newTag->slug = Str::of($newTag->name)->slug('-');
+
+            $newTag->save();
+        }
+    }
+}
